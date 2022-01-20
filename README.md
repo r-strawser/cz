e5ee31dd8e4139b1b9c0aa27411fa5fc6b73f14a
Ryan Strawser <strawser.ryan@gmail.com>
2022-01-20 09:40:52 -0500

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# cz
+CryptoZoo subgraph for monitoring hatch events.
+
+Go to "Create Subgraph" on "My Subgraphs" and to create your own deployment.
+
+# Referenced instructions
+
+create subgraph with studio (recommended for ETH): https://thegraph.com/docs/en/studio/deploy-subgraph-studio/
+
+create subgraph through hosted service (recommended for other supported chains after deploying on ETH with the studio): https://thegraph.com/docs/en/hosted-service/deploy-subgraph-hosted/
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,16 +1,15 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  CryptoZooBaseAnimal,
-  Approval,
-  ApprovalForAll,
-  OwnershipTransferred,
-  Transfer,
-  animalTokenURIUpdated,
-  burned,
+  Transfer as TransferEvent,
+  CryptoZooBaseAnimal as AnimalContract,
   hatched
 } from "../generated/CryptoZooBaseAnimal/CryptoZooBaseAnimal"
-import { ExampleEntity } from "../generated/schema"
 
+import { 
+  /*ExampleEntity, */HatchedEvent, CryptoZooBaseAnimal, ZooKeeper 
+} from "../generated/schema"
+
+/*
 export function handleApproval(event: Approval): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
@@ -65,17 +64,55 @@ export function handleApproval(event: Approval): void {
   // - contract.tokenURI(...)
   // - contract.totalSupply(...)
 }
+*/
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+export function handleTransfer(event: TransferEvent): void {
+  let token = CryptoZooBaseAnimal.load(event.params.tokenId.toString());
+  if (!token) {
+   token = new CryptoZooBaseAnimal(event.params.tokenId.toString());
+   token.creator = event.params.to.toHexString();
+   token.tokenID = event.params.tokenId;
+   token.createdAtTimestamp = event.block.timestamp;
+  
+   let tokenContract = AnimalContract.bind(event.address);
+   token.metadataURI = tokenContract.tokenURI(event.params.tokenId);
+  }
+  token.owner = event.params.to.toHexString();
+  token.save();
+  
+  let user = ZooKeeper.load(event.params.to.toHexString());
+  if (!user) {
+   user = new ZooKeeper(event.params.to.toHexString());
+   user.save();
+  }
+}
 
-export function handleTransfer(event: Transfer): void {}
+export function handlehatched(event: hatched): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = HatchedEvent.load(event.transaction.from.toHex())
 
-export function handleanimalTokenURIUpdated(
-  event: animalTokenURIUpdated
-): void {}
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new HatchedEvent(event.transaction.from.toHex())
 
-export function handleburned(event: burned): void {}
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+  }
+  entity.tokenID = event.params.tokenId
 
-export function handlehatched(event: hatched): void {}
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // BigInt and BigDecimal math are supported
+  entity.hatchedAtTimestamp = event.block.timestamp
+
+  // Entity fields can be set based on event parameters
+  entity.owner = event.params.toAddress
+  entity.contractAddress = event.params.contractAddress
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+}
